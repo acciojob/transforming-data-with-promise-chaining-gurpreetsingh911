f7ea110c637fbbcf1e6a846b8f3102724814e0a9
Dragon_Fly <143097451+gurpreetsingh911@users.noreply.github.com>
2025-06-02 12:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,43 +11,52 @@ let promise = new Promise((resolve, reject) => {
 
 
 setTimeout(() => {
-output.innerHTML = (`Result: ${value}`)
+ output.innerHTML = (`Result:1 ${value}`)
 },2000)
 resolve();
 })
 
+ let second = value * 2;
 let sencProm = await new Promise((resolve, reject) => {
-    let second = value * 2;
+   
     setTimeout(() =>
-    output.innerHTML = (`Result: ${second}`)
+   output.innerHTML = (`Result:2 ${second}`)
     , 2000);
-    resolve();
+    resolve(second);
+    //Multiply the number by 2 and display it in the div as Result: <number>.
+
   });
 
+let third = second - 3
 let thirdPrm = await new Promise((resolve, reject) => {
-    let third = value - 3
+    
     setTimeout(() => 
-  	output.innerHTML = (`Result: ${third}`)
+  output.innerHTML = (`Result3: ${third}`)
     , 1000);
     resolve();
+    //subtract 3 from the number and display it as Result: <number>.
 });
-
+ let forth = third /2;
 let forthPrm = await new Promise((resolve, reject) => {
-  let forth = value /2;
+ 
   setTimeout(() =>
-	output.innerHTML = (`Result: ${forth}`)
+output.innerHTML = (`Result4: ${forth}`)
     , 1000);
     resolve();
+    //Divide the number by 2 and display it as Result: <number>.
+
   });
   
+  let five = forth + 10;
 let fivePrm = await new Promise((resolve, reject) => {
-  let five = value + 10;
+  
   setTimeout(() =>
-  output.innerHTML = (`Result: ${five}`)
+  output.innerHTML = (`Result5: ${five}`)
     , 1000);
     resolve();
+    //Add 10 to the number and display it as Final Result: <number>.
+
  });
-  
   
 })
 
